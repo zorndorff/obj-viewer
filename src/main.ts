d6518a3d7d521ee1f95984c5bdd576c5278486df
Zac Orndorff <zorndorff@gmail.com>
2021-07-24 08:59:53 -0500

    camera update

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
 import './style.css';
-import broom from './bathroom_plan.obj?url';
-import broomMtl from './bathroom_plan.mtl?url';
 import 'aframe';
 
 const app = document.querySelector('#app')!;
@@ -9,10 +7,13 @@ app.innerHTML = `
   <div id="viewer">
     <a-scene background="color: #ECECEC">
     <a-assets>
-      <a-asset-item id="viewedModel" src="${broom}"></a-asset-item>
-      <a-asset-item id="viewedModelMtl" src="${broomMtl}"></a-asset-item>
+      <a-asset-item id="viewedModel" src="bathroom_plan.obj"></a-asset-item>
+      <a-asset-item id="viewedModelMtl" src="bathroom_plan.mtl"></a-asset-item>
     </a-assets>
     <a-entity obj-model="obj: #viewedModel;  mtl: #viewedModelMtl;"></a-entity>
+    <a-entity id="rig" position="25 10 0">
+      <a-entity id="camera" camera look-controls></a-entity>
+    </a-entity>
     </a-scene>
   </div>
 `;
