1c8b42bcedbf2b1d75ff878c8452aadfcb12e5ca
Zac Orndorff <zorndorff@gmail.com>
2021-07-24 08:12:59 -0500

    use mtl for textures

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import './style.css';
 import broom from './bathroom_plan.obj?url';
+import broomMtl from './bathroom_plan.mtl?url';
 import 'aframe';
 
 const app = document.querySelector('#app')!;
@@ -9,8 +10,9 @@ app.innerHTML = `
     <a-scene background="color: #ECECEC">
     <a-assets>
       <a-asset-item id="viewedModel" src="${broom}"></a-asset-item>
+      <a-asset-item id="viewedModelMtl" src="${broomMtl}"></a-asset-item>
     </a-assets>
-    <a-entity obj-model="obj: #viewedModel;"></a-entity>
+    <a-entity obj-model="obj: #viewedModel;  mtl: #viewedModelMtl""></a-entity>
     </a-scene>
   </div>
 `;
